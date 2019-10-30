63523a643a070dda2edc0db11cd77af09212bfaf
jsrudd <56898694+jsrudd@users.noreply.github.com>
2019-10-30 14:07:36 +1000

    Add files via upload

---
+++ Bulk Azure AD Attribute Update.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module AzureAD
+Connect-AzureAD
+$Users = #Enter CSV FilePAth here#
+
+ForEach ($User in $Users) {
+    $UID = $User.ObjectID    
+    $Dept = $User.Department
+    $Loc = $User.Location
+    Set-AzureADUser -ObjectID $UID -Department $Dept -PhysicalDeliveryOfficeName $Loc
+    Start-Sleep -s 5
+    }
